d3fa3cc4342fb2f247b2a08b54f3a894babc2ae9
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2022-10-07 09:31:18 +0800

    part b done

--- Example Class 2/Part b/Codes/entryGate.java
+++ Example Class 2/Part b/Codes/entryGate.java
@@ -1,17 +1,23 @@
 import java.util.*; 
+import java.io.File; // Import the File class
+import java.io.FileWriter;
+import java.io.IOException; // Import the IOException class to handle errors
+
 
 class entryGate{
     static int size;
     static int edges;
-    public static void main(String arg[])   { 
-    Scanner sc = new Scanner(System.in);
-    
+    public static void main(String arg[]) throws IOException   {
+      for (size = 200; size <= 200; size++) {
+        edges = ((size * size) - size); 
 
-    //Generate Matrix from line 9 to line 62
+    /*
+     Scanner sc = new Scanner(System.in);
+    //Generate Matrix from line 9 to line 26
     System.out.println("Please enter the number of vertices: ");
     Scanner scan = new Scanner(System.in);
     size = scan.nextInt();
-    System.out.println("Please enter the number of edges: ");
+    System.out.println("Please enter the number fo edges: ");
     edges= scan.nextInt();
     if(edges>(size*size)-size){
       int tmp = (size*size)-size;
@@ -19,6 +25,8 @@ class entryGate{
       System.out.println("Your edges is too large");
       return;
     }
+     */
+    
     Random rand = new Random();
     int upperbound = 10; 
     int graph[][] = new int[size][size];
@@ -105,14 +113,16 @@ class entryGate{
       }   
     }
         //end code here - matrix is generated
-
-        System.out.println("Total edges: "+totalEdges);
+/*
+ * System.out.println("Total edges: "+totalEdges);
         for(int i=0;i<size;i++) {
             for(int j=0;j<size;j++) {
               System.out.print(graph[i][j]+" ");
             }
             System.out.println();
           }
+ */
+        
 
        
         sizeOfArray = sizeOfArray+1;
@@ -136,23 +146,41 @@ class entryGate{
           int first = array[++sizeOfArray];
           int second = array[++sizeOfArray];
           int randtmp = rand.nextInt(10-1)+1;
-          System.out.printf("adj_list.get(%d).add(new Node(%d,%d)", first, second, randtmp);
-          System.out.println("");
+          //System.out.printf("adj_list.get(%d).add(new Node(%d,%d)", first, second, randtmp);
+          //System.out.println("");
             adj_list.get(first).add(new Node(second, randtmp)); 
             if(sizeOfArray>=oldSize-1){
                 break;
             }
         }
         
-            
+        //System.out.println("Total edges: " + totalEdges);
+      long start = System.nanoTime();
         // call Dijkstra's algo method  
         Graph_pq dpq = new Graph_pq(V); 
         dpq.algo_dijkstra(adj_list, source); 
+        long end = System.nanoTime();
+        long elapsedTime = end - start;
+      System.out.println("Number of vertices: "+size);
+      System.out.println("Number of edges: "+totalEdges);
+      System.out.println("In nanoseconds : " + elapsedTime);
+      double elapsedTimeInSecond = (double) elapsedTime / 1000000000;
+      System.out.println("In seconds: " + elapsedTimeInSecond);
+      File csvFile = new File("TimeanalysisPartialGraphDifferent|V|.csv");
+      FileWriter fileWriter = new FileWriter(csvFile, true);
+      // fileWriter.append("Number of vertices" + "," + "Number of edges" + "," +
+      // "Time in nanoseconds" + "," + "Time in seconds");
+      fileWriter.append((System.getProperty("line.separator")));
+      fileWriter.append(size + "," + totalEdges + "," + elapsedTime + "," + elapsedTimeInSecond);
+      fileWriter.close();
    
-        // Print the shortest path from source node to all the nodes 
+        /*
+         *  // Print the shortest path from source node to all the nodes 
         System.out.println("The shorted path from source node("+source+") to other nodes:"); 
         System.out.println("Node\t\t" + "Predecessor\t\t" + "Distance");
         for (int i = 0; i < dpq.dist.length; i++) 
             System.out.println(" " + i + " \t\t     "  + dpq.pi[i]+ " \t\t\t   "  + dpq.dist[i]);
+         */
+      }
     } 
 } 
