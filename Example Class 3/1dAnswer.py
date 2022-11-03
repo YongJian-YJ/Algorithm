b6758e07a2559ab5ecf8f4c5e3ed477c17f09762
hibye0714 <48339717+YongJian-YJ@users.noreply.github.com>
2022-11-03 19:35:19 +0800

    changed variable name

--- Example Class 3/1dAnswer.py
+++ Example Class 3/1dAnswer.py
@@ -1,21 +1,24 @@
-def unboundedKnapsack(W, n, val, wt):
+def unboundedKnapsack(C, n, profit, weight):
  
-    # dp[i] is going to store maximum
-    # value with knapsack capacity i.
-    dp = [0 for i in range(W + 1)]
+    #C: capacity of knapsack
+    #n: number of items
+    # dp[i] is going to store maximum value with knapsack capacity i
+    dp = [0 for i in range(C + 1)]
 
-    # Fill dp[] using above recursive formula
-    for i in range(W + 1):
+    # Recursive Formula for finding the maximum profit
+    for i in range(C + 1):
         for j in range(n):
-            if (wt[j] <= i):
-                dp[i] = max(dp[i], dp[i - wt[j]] + val[j])
+            if (weight[j] <= i):
+                dp[i] = max(dp[i], dp[i - weight[j]] + profit[j])
  
-    return dp[W]
+    return dp[C]
  
 # Driver program
-W = 14
-val = [7, 6, 9]
-wt = [4, 6, 8]
-n = len(val)
+C = 14
+profit = [7, 6, 9]
+weight = [4, 6, 8]
+n = len(profit)
  
-print(unboundedKnapsack(W, n, val, wt))
+print(unboundedKnapsack(C, n, profit, weight))
+
+
--- Example Class 3/2dAnswer.py
+++ Example Class 3/2dAnswer.py
@@ -1,58 +1,58 @@
-def printknapSack(W, wt, val, n):
+def printknapSack(C, weight, profit, n):
     K = [[0 for i in range(n + 1)]
-            for w in range(W + 1)]
+            for C in range(C + 1)]
 			
 	# Build table K[][] in bottom up manner
-    for w in range(W + 1):
+    for C in range(C + 1):
         for i in range(n + 1):
-            if i == 0 or w == 0:
-                K[w][i] = 0
-            elif wt[i-1] <= w:
-                K[w][i] = max(val[i - 1]+ K[w - wt[i-1]][i], K[w][i-1])               
+            if i == 0 or C == 0:
+                K[C][i] = 0
+            elif weight[i-1] <= C:
+                K[C][i] = max(profit[i - 1]+ K[C - weight[i-1]][i], K[C][i-1])               
             else:
-                K[w][i] = K[w][i-1]
+                K[C][i] = K[C][i-1]
 
     #print k table
-    for w in range(W + 1):
+    for C in range(C + 1):
         for i in range(n + 1):
-            print(K[w][i],end=" ")
+            print(K[C][i],end=" ")
         print()
 
 
     # stores object taken
-    i=W
+    i=C
     j=n
-    idxes_list = []
+    objectArray = []
     while(i>0 and j>0):
         if(j>0 and K[i][j]!=K[i][j-1]):
-            idxes_list.append(j)
-            i=i-wt[j-1]
-        elif(j==0 and i>wt[j]):
-            idxes_list.append(j)
-            i=i-wt[j-1]
+            objectArray.append(j)
+            i=i-weight[j-1]
+        elif(j==0 and i>weight[j]):
+            objectArray.append(j)
+            i=i-weight[j-1]
         else:
             j=j-1
     
-    print ("\nHighest possible profit is: ", K[W][n], " and the objects to be taken are: ", *idxes_list)
+    print ("\nHighest possible profit is: ", K[C][n], " and the objects to be taken are: ", *objectArray)
     
     
   
 
 
 # Driver code
-val = [7, 6, 9]
-wt = [4, 6, 8]
-W = 14
-n = len(val)
+profit = [7, 6, 9]
+weight = [4, 6, 8]
+C = 14
+n = len(profit)
 
-val2 = [7 ,6 ,9]
-wt2 = [5 ,6 , 8]
+profit2 = [7 ,6 ,9]
+weight2 = [5 ,6 , 8]
 
 print ("Question 3: ")
-printknapSack(W, wt, val, n)
+printknapSack(C, weight, profit, n)
 
 print()
 print()
 
 print ("Question 4: ")
-printknapSack(W, wt2, val2, n)
+printknapSack(C, weight2, profit2, n)
